b4c418bc0350602c35c53c8e9a1745ee6ebf8e83
Jerish PJ <jerish.pj@hotmail.com>
2025-10-06 21:02:23 +0530

    Code refactoring

--- JerishPJ.FizzBuzz/JerishPJ.FizzBuzz.WebAPI/Controllers/ErrorController.cs
+++ JerishPJ.FizzBuzz/JerishPJ.FizzBuzz.WebAPI/Controllers/ErrorController.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace JerishPJ.FizzBuzz.WebAPI.Controllers
 {
@@ -14,23 +7,6 @@ namespace JerishPJ.FizzBuzz.WebAPI.Controllers
     [ApiController]
     public class ErrorController : ControllerBase
     {
-        [Route("/error-local-development")]
-        public IActionResult ErrorLocalDevelopment(
-        [FromServices] IWebHostEnvironment webHostEnvironment)
-        {
-            if (webHostEnvironment.EnvironmentName != "Development")
-            {
-                throw new InvalidOperationException(
-                    "This shouldn't be invoked in non-development environments.");
-            }
-
-            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
-
-            return Problem(
-                detail: context.Error.StackTrace,
-                title: context.Error.Message);
-        }
-
         [Route("/error")]
         public IActionResult Error() => Problem();
     }
